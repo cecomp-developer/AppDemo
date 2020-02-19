ad27094b5a32bb2c66e59ddf2eed520c28ddc42a
Hugo Rojas <hugorojas@live.com.mx>
2020-02-19 16:00:54 -0600

    Appdemo

---
+++ AppDemo.DAL/Repository/GenericRepository.cs
@@ -0,0 +1,182 @@
+﻿using AppDemo.DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace AppDemo.DAL.Repository
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly UsuariosDbContext  _context;
+
+        public GenericRepository(UsuariosDbContext context)
+        {
+            this._context = context;
+        }
+
+        public IQueryable<TEntity> Query()
+        {
+            return _context.Set<TEntity>().AsQueryable();
+        }
+
+        public IQueryable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>();
+        }
+
+        public virtual async Task<ICollection<TEntity>> GetAllAsync()
+        {
+            return await _context.Set<TEntity>().ToListAsync();
+        }
+
+        public virtual TEntity Get(object id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public virtual async Task<TEntity> GetAsync(object id)
+        {
+            return await _context.Set<TEntity>().FindAsync(id);
+        }
+
+        public virtual TEntity Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public virtual async Task<TEntity> AddAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public virtual TEntity Find(Expression<Func<TEntity, bool>> match)
+        {
+            return _context.Set<TEntity>().SingleOrDefault(match);
+        }
+
+        public virtual async Task<TEntity> FindAsync(Expression<Func<TEntity, bool>> match)
+        {
+            return await _context.Set<TEntity>().SingleOrDefaultAsync(match);
+        }
+
+        public ICollection<TEntity> FindAll(Expression<Func<TEntity, bool>> match)
+        {
+            return _context.Set<TEntity>().Where(match).ToList();
+        }
+
+        public async Task<ICollection<TEntity>> FindAllAsync(Expression<Func<TEntity, bool>> match)
+        {
+            return await _context.Set<TEntity>().Where(match).ToListAsync();
+        }
+
+        public virtual void Delete(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public virtual async Task<int> DeleteAsync(TEntity entity)
+        {
+            _context.Set<TEntity>().Remove(entity);
+            return await _context.SaveChangesAsync();
+        }
+
+        public virtual TEntity Update(TEntity entity, object key)
+        {
+            if (entity == null)
+                return null;
+            TEntity exist = _context.Set<TEntity>().Find(key);
+            if (exist != null)
+            {
+                _context.Entry(exist).CurrentValues.SetValues(entity);
+                _context.SaveChanges();
+            }
+            return exist;
+        }
+
+        public virtual async Task<TEntity> UpdateAsync(TEntity entity, object key)
+        {
+            if (entity == null)
+                return null;
+            TEntity exist = await _context.Set<TEntity>().FindAsync(key);
+            if (exist != null)
+            {
+                _context.Entry(exist).CurrentValues.SetValues(entity);
+                await _context.SaveChangesAsync();
+            }
+            return exist;
+        }
+
+        public int Count()
+        {
+            return _context.Set<TEntity>().Count();
+        }
+
+        public async Task<int> CountAsync()
+        {
+            return await _context.Set<TEntity>().CountAsync();
+        }
+
+        public virtual IQueryable<TEntity> FindBy(Expression<Func<TEntity, bool>> predicate)
+        {
+            IQueryable<TEntity> query = _context.Set<TEntity>().Where(predicate);
+            return query;
+        }
+
+        public virtual async Task<ICollection<TEntity>> FindByAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().Where(predicate).ToListAsync();
+        }
+
+        public IQueryable<TEntity> GetAllIncluding(params Expression<Func<TEntity, object>>[] includeProperties)
+        {
+            IQueryable<TEntity> queryable = GetAll();
+            foreach (Expression<Func<TEntity, object>> includeProperty in includeProperties)
+            {
+
+                queryable = queryable.Include<TEntity, object>(includeProperty);
+            }
+            return queryable;
+        }
+
+        public IQueryable<TEntity> Include(params Expression<Func<TEntity, object>>[] includeExpressions)
+        {
+            var dbSet = _context.Set<TEntity>();
+
+            IQueryable<TEntity> query = null;
+            foreach (var includeExpression in includeExpressions)
+            {
+                query = dbSet.Include(includeExpression);
+            }
+
+            return query ?? dbSet;
+        }
+
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ AppDemo.Security/Hash.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AppDemo.Security
+{
+    public class Hash
+    {
+        public static string GenerarHash(string password, string sal)
+        {
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+                password: password,
+                salt: Encoding.UTF8.GetBytes(sal),
+                prf: KeyDerivationPrf.HMACSHA1,
+                iterationCount: 10000,
+                numBytesRequested: 256 / 8));
+            return hashed;
+        }
+
+        public static byte[] GenerarSalByte()
+        {
+            byte[] sal = new byte[128 / 8];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(sal);
+            }
+            return sal;
+        }
+
+        public static string GenerarSalString()
+        {
+            byte[] sal = new byte[128 / 8];
+
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(sal);
+            }
+            return Convert.ToBase64String(sal);
+        }
+
+        public static bool VerificarHashPassword(string password, string sal, string hash)
+        {
+            if (GenerarHash(password, sal) == hash)
+                return true;
+            else
+                return false;
+        }
+    }
+}
